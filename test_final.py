bceaa5c9a5929631bd422cbbbd5c3789e146b515
JonY0ung <dyoung26@terpmail.umd.edu>
2024-05-09 15:57:37 -0400

    CHANGED TO PROPERLY FORMAT PYTEST

--- test_final.py
+++ test_final.py
@@ -1,5 +1,5 @@
 "INST326 - Final Project Test Cases - DeJon Young, Samuel Conteh"
-import unittest
+
 import pytest
 from final import User
 
@@ -18,13 +18,26 @@ def test_authenticate():
 # def test_access_user():
 
 # def test_recommend_career_paths():
-def test_recomend_career_paths():
-    User.add_courses({'INST341': {'name': 'Introduction to Digital Curation', 'career_paths': ['Data Analyst', 'Digital Archivist'], 'credits': 'N/A'}})
-    User.recommend_career_paths ({'INST341': {'name': 'Introduction to Digital Curation', 'career_paths': ['Data Analyst', 'Digital Archivist'], 'credits': 'N/A'}})
+User('sconteh', 'PythonIsCool123!', 'Samuel', 'Conteh')
+    
+    # Add courses to the user
+User.add_courses({'INST341': {'name': 'Introduction to Digital Curation', 'career_paths': ['Data Analyst', 'Digital Archivist'], 'credits': 'N/A'}})
+    
+# Get recommended career paths
+recommended_paths = User.recommend_career_paths({'INST341': {'name': 'Introduction to Digital Curation', 'career_paths': ['Data Analyst', 'Digital Archivist'], 'credits': 'N/A'}})
+    
+# Define expected career paths
+expected_paths = {'Data Analyst', 'Digital Archivist'}
+    
+# Check if the recommended paths match the expected paths
+assert set(recommended_paths) == expected_paths
 
    
 # def test_change_password
 def test_change_password():
+    # Create a user object with the initial password
+    User('sconteh', 'PythonIsCool123!', 'Samuel', 'Conteh')
+    
     # Change the password
     old_password = 'PythonIsCool123!'
     new_password = 'object_orient'
@@ -34,8 +47,5 @@ def test_change_password():
     # Create a new user object with the updated password
     updated_user = User('sconteh', 'object_orient', 'Samuel', 'Conteh')
     
-     # Verify authentication with the new password
+    # Verify authentication with the new password
     assert updated_user.authenticate(new_password)
-
-if __name__ == '__main__':
-    unittest.main()
