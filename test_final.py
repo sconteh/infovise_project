e01226162d7a43db88ac0b8aff65e81d5291f840
JonY0ung <dyoung26@terpmail.umd.edu>
2024-05-09 15:36:04 -0400

    Update test_final.py

--- test_final.py
+++ test_final.py
@@ -1,4 +1,5 @@
-
+"INST326 - Final Project Test Cases - DeJon Young, Samuel Conteh"
+import unittest
 import pytest
 from final import User
 
@@ -17,5 +18,24 @@ def test_authenticate():
 # def test_access_user():
 
 # def test_recommend_career_paths():
+def test_recomend_career_paths():
+    User.add_courses({'INST341': {'name': 'Introduction to Digital Curation', 'career_paths': ['Data Analyst', 'Digital Archivist'], 'credits': 'N/A'}})
+    User.recommend_career_paths ({'INST341': {'name': 'Introduction to Digital Curation', 'career_paths': ['Data Analyst', 'Digital Archivist'], 'credits': 'N/A'}})
 
-# def test_
+   
+# def test_change_password
+def test_change_password():
+    # Change the password
+    old_password = 'PythonIsCool123!'
+    new_password = 'object_orient'
+    User.change_password(old_password, new_password)
+    
+    # Verify that the password has been changed
+    # Create a new user object with the updated password
+    updated_user = User('sconteh', 'object_orient', 'Samuel', 'Conteh')
+    
+     # Verify authentication with the new password
+    assert updated_user.authenticate(new_password)
+
+if __name__ == '__main__':
+    unittest.main()
